f63a743fe02a0f4b3dac079484726950aececf31
Top <Top@3-15>
2024-10-11 16:51:22 +0500

    11.10.2024

--- main.cpp
+++ main.cpp
@@ -1,96 +1,204 @@
 #include <iostream>
 #include <Windows.h>
 #include <random>
+
+
+
+void PrintHelloWorld()
+{
+	std::cout << "Hello world\n";
+}
+
+void PrintNumber(int number)
+{
+	std::cout << number;
+}
+
 int main()
 {
-	//setlocale(LC_ALL, "ru");
+	setlocale(LC_ALL, "ru");
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 	srand(time(NULL));
-	
-	int spacing1;
-	int spacing2;
-	int weight;
-	float fuel = 500;
-	int consumption;
-	int refueling;
-	
-	do
-	{
-		std::cout << "\nВведите вес блоков питания(кг): ";
-		std::cin >> weight;
-		std::cout << "\nВведите расстояние от точки A до точки B(км): ";
-		std::cin >> spacing1;
-		std::cout << "\nВведите расстояние от точки B до точки C(км): ";
-		std::cin >> spacing2;
 
-		if (weight <= 400 && spacing1 < 251 && spacing2 < 251 && spacing1 > 0 && spacing2 > 0 && weight > 0)
-		{
-			consumption = spacing1 * 2;
-			fuel = fuel - consumption;
-			consumption = spacing2 * 2;
-			refueling = consumption - fuel;
-			std::cout << "\nМожно ехать, нужно дозаправить в точке B: " << refueling << " литров";
-			
-		}
-		else if (weight <= 400 && spacing1 > 251 && spacing2 > 251 && spacing1 < 0 && spacing2 < 0 && weight > 0)
-		{
-			std::cout << "\n Ехать нельзя";
-			
-		};
-		
-		if (weight <= 900 && spacing1 < 101 && spacing2 < 101 && spacing1 > 0 && spacing2 > 0 && weight > 0)
-		{
-			consumption = spacing1 * 5;
-			fuel = fuel - consumption;
-			consumption = spacing2 * 5;
-			refueling = consumption - fuel;
-			std::cout << "\nМожно ехать, нужно дозаправить в точке B: " << refueling << " литров";
-			
-		}
-		else
-		{
-			std::cout << "\n Ехать нельзя";
-			
-		};
-		
-		if (weight <= 1400 && spacing1 < 62.6 && spacing2 < 62.6 && spacing1 > 0 && spacing2 > 0 && weight > 0)
-		{
-			consumption = spacing1 * 8;
-			fuel = fuel - consumption;
-			consumption = spacing2 * 8;
-			refueling = consumption - fuel;
-			std::cout << "\nМожно ехать, нужно дозаправить в точке B: " << refueling << " литров";
-			
-		}
-		else
-		{
-			std::cout << "\n Ехать нельзя";
-		};
-		
-		if (weight < 1900 && spacing1 < 45.4 && spacing2 < 45.4 && spacing1 > 0 && spacing2 > 0 && weight > 0)
-		{
-			consumption = spacing1 * 11;
-			fuel = fuel - consumption;
-			consumption = spacing2 * 11;
-			refueling = consumption - fuel;
-			std::cout << "\nМожно ехать, нужно дозаправить в точке B: " << refueling << " литров";
-			
-		}
-		else
-		{
-			std::cout << "\n Ехать нельзя";
-		
-		};
-	} while (spacing1 <= 0 || spacing2 <= 0 || weight <= 0);
+	PrintHelloWorld();
+	PrintNumber(7);
 
-
-
-
-	
 	return 0;
 }
 
+
+
+//chiselki-------------------------------------------------------------------
+/*const int size = 20;
+	int arr[size]{};
+	int zeroCount = 0;
+	int summPositive = 0;
+	int summNegative = 0;
+	int ostatokOtDeleniya = 0;
+	std::cout << "chisla massiva: ";
+	for (int i = 0; i < size; i++)
+	{
+		arr[i] = rand() % 21 - 10;
+		if (arr[i] == 0)
+		{
+			zeroCount++;
+		};
+		std::cout << arr[i] << " ";
+	}
+	for (int i = 0; i < size; i++)
+	{
+		if (arr[i] > 0)
+		{
+			summPositive = summPositive + arr[i];
+		};
+	}
+	for (int i = 0; i < size; i++)
+	{
+		if (arr[i] < 0)
+		{
+			summNegative = summNegative + arr[i];
+		}
+	}
+	std::cout << "\n\nchetnie chisla: ";
+	for (int i = 0; i < size; i++)
+	{
+		if (arr[i]%2 == 0 && arr[i] != 0)
+		{
+			std::cout << arr[i] << " ";
+		}
+	}
+	std::cout << "\n\nnechetnie chisla: ";
+	for (int i = 0; i < size; i++)
+	{
+		if (arr[i]%2 != 0 && arr[i] != 0)
+		{
+			std::cout << arr[i] << " ";
+		}
+	}
+	std::cout << "\n\nsumma polojitel'nih chisel: " << summPositive << "\n";
+	std::cout << "\nsumma otricatel'nih chisel: " << summNegative << "\n";
+	std::cout << "\nkolichestvo nulei: " << zeroCount << "\n";*/
+//-----------------------------------------------------------------------------------------------------------
+//sovpadenie chisel randoma i vvoda polzovatelya//dodelat'
+//const int size = 10;
+//int comp[size]{};
+//int user[size]{};
+
+//for (int i = 0; i < size; i++)
+//{
+
+//	int count = 0;
+//	do
+//	{
+//		std::cout << "Введите десять цифр от -5 до 8: ";
+//		std::cin >> user[i];
+//	} while (user[i] < -5 || user[i] > 8);
+//	do
+//	{
+//		comp[i] = rand() % 14 - 5;
+//		std::cout << comp[i] << "\n";
+//		std::cout << user[i];
+//		if (user[i] == comp[i])
+//		{
+//			count++;
+//		}
+//	} while (comp[i] != user[i]);
+//	if (count > 0)
+//	{
+//		std::cout << "molodec";
+//	};
+//Массивы 11.10.24
+//int a = 3;
+//int arr[9];
+//arr[3] = 4;
+//const int size = 9;
+//int arr[size]{ 4,3,1,3 };
+//std::cout << arr[9];
+//for (int i = 0; i < size; i++)
+//{
+//	arr[i] = i;
+//	std::cout << arr[i] << " ";
+//};
+
+//Ехать или не ехать
+//int spacing1;
+//int spacing2;
+//int weight;
+//float fuel = 500;
+//int consumption;
+//int refueling;
+
+//do
+//{
+//	std::cout << "\nВведите вес блоков питания(кг): ";
+//	std::cin >> weight;
+//	std::cout << "\nВведите расстояние от точки A до точки B(км): ";
+//	std::cin >> spacing1;
+//	std::cout << "\nВведите расстояние от точки B до точки C(км): ";
+//	std::cin >> spacing2;
+
+//	if (weight <= 400 && spacing1 < 251 && spacing2 < 251 && spacing1 > 0 && spacing2 > 0 && weight > 0)
+//	{
+//		consumption = spacing1 * 2;
+//		fuel = fuel - consumption;
+//		consumption = spacing2 * 2;
+//		refueling = consumption - fuel;
+//		std::cout << "\nМожно ехать, нужно дозаправить в точке B: " << refueling << " литров";
+
+//	}
+//	else if (weight <= 400 && spacing1 > 251 && spacing2 > 251 && spacing1 < 0 && spacing2 < 0 && weight > 0)
+//	{
+//		std::cout << "\n Ехать нельзя";
+
+//	};
+
+//	if (weight <= 900 && spacing1 < 101 && spacing2 < 101 && spacing1 > 0 && spacing2 > 0 && weight > 0)
+//	{
+//		consumption = spacing1 * 5;
+//		fuel = fuel - consumption;
+//		consumption = spacing2 * 5;
+//		refueling = consumption - fuel;
+//		std::cout << "\nМожно ехать, нужно дозаправить в точке B: " << refueling << " литров";
+
+//	}
+//	else
+//	{
+//		std::cout << "\n Ехать нельзя";
+
+//	};
+
+//	if (weight <= 1400 && spacing1 < 62.6 && spacing2 < 62.6 && spacing1 > 0 && spacing2 > 0 && weight > 0)
+//	{
+//		consumption = spacing1 * 8;
+//		fuel = fuel - consumption;
+//		consumption = spacing2 * 8;
+//		refueling = consumption - fuel;
+//		std::cout << "\nМожно ехать, нужно дозаправить в точке B: " << refueling << " литров";
+
+//	}
+//	else
+//	{
+//		std::cout << "\n Ехать нельзя";
+//	};
+
+//	if (weight < 1900 && spacing1 < 45.4 && spacing2 < 45.4 && spacing1 > 0 && spacing2 > 0 && weight > 0)
+//	{
+//		consumption = spacing1 * 11;
+//		fuel = fuel - consumption;
+//		consumption = spacing2 * 11;
+//		refueling = consumption - fuel;
+//		std::cout << "\nМожно ехать, нужно дозаправить в точке B: " << refueling << " литров";
+
+//	}
+//	else
+//	{
+//		std::cout << "\n Ехать нельзя";
+
+//	};
+//} while (spacing1 <= 0 || spacing2 <= 0 || weight <= 0);
+
+//-------------------------------------------------------------
 //Конвертация валюты
 //int rub;
 //int eur = 103.19;
